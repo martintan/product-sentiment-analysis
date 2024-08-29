b2d0a3e3d50e6aeaa1e9769bd80a580981ef38cf
martin <martinwdtan@gmail.com>
2024-08-29 18:53:20 +0800

    refactor: wrap logic in main function

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pandas as pd
 from models import DatasetConfig
 
-# configure the dataset here
-config = DatasetConfig(
-    csv_path="./product_reviews_data.csv",
-    review_text_column="reviews.text",
-    rating_column="reviews.rating",
-    max_rows=100,
-)
+def main():
+    # configure the dataset here
+    config = DatasetConfig(
+        csv_path="./product_reviews_data.csv",
+        review_text_column="reviews.text",
+        rating_column="reviews.rating",
+        max_rows=100,
+    )
 
-df = pd.read_csv(config.csv_path, nrows=config.max_rows)
-# only get the relevant columns
-df = df[[config.review_text_column, config.rating_column]]
-# just make sure the column names are consistent
-df = df.rename(
-    columns={
-        config.review_text_column: "review_text",
-        config.rating_column: "rating",
-    }
-)
+    df = pd.read_csv(config.csv_path, nrows=config.max_rows)
+    # only get the relevant columns
+    df = df[[config.review_text_column, config.rating_column]]
+    # just make sure the column names are consistent
+    df = df.rename(
+        columns={
+            config.review_text_column: "review_text",
+            config.rating_column: "rating",
+        }
+    )
 
-print(df)
+    print(df)
+
+if __name__ == "__main__":
+    main()
